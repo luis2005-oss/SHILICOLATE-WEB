89d869ab0416982fcd7f652d233f62dffe3c1901
luis2005-oss <solanocortezluisalfredo77@gmail.com>
2026-04-02 00:57:54 -0500

    ..

--- src/components/pages/contactPage.jsx
+++ src/components/pages/contactPage.jsx
@@ -1,14 +1,12 @@
 import { MyTemplate } from "../templates/myTemplate"
 import { ContactHero } from "../organisms/contact/contactHero"
 import { ContactFormSection } from "../organisms/contact/ContactFormSection"
-import { ContactMapSection } from "../organisms/contact/ContactMapSection"
 
 function ContactPage() {
   return (
     <MyTemplate>
       <ContactHero />
       <ContactFormSection />
-      <ContactMapSection />
     </MyTemplate>
   )
 }
