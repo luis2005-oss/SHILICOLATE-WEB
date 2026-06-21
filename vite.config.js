1ae7ca16a4bd6a9d61ed5387e37e72a02a3a1a1e
luis2005-oss <solanocortezluisalfredo77@gmail.com>
2026-06-21 15:01:32 -0500

    feat: agregar configuración de Vercel y optimizar la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          icons: ['react-icons'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500,
+  },
 })
